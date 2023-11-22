d9147e1838d6e086f444f755b776b6a24d02cd66
aeross <andrewpratama22@gmail.com>
2023-11-22 15:17:50 +1100

    added home component

--- client/src/routers/Index.jsx
+++ client/src/routers/Index.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Game from '../views/Game';
+import Home from '../views/Home';
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Game />,
+        element: <Home />,
+    },
+    {
+        path: "/game",
+        element: <Game />
     }
 ])
 
---
+++ client/src/views/Home.jsx
@@ -0,0 +1,13 @@
+export default function Home() {
+    return (<>
+        <div>
+            <button className="border-2 rounded-md bg-slate-100 py-2 px-4">New Game</button>
+            <button className="border-2 rounded-md bg-slate-100 py-2 px-4">Join Game</button>
+
+            <div>
+            <input type="text" />
+            <button>Join</button>
+            </div>
+        </div>
+    </>)
+}
