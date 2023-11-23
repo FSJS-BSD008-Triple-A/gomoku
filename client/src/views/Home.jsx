7b20f84c8aea72f30a56ec33ce12b4c97e404cde
amaralkaff <tpamangly@gmail.com>
2023-11-23 12:51:11 +0700

    add button styling

--- client/src/views/Home.jsx
+++ client/src/views/Home.jsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router-dom";
 import { reset } from "../slices/gomokuSlice";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 
 export default function Home() {
